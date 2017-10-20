946eac2582904547aefb682fc000faf6559f22d4
Robert Lord <robert@lord.io>
2017-10-20 00:34:54 -0500

    Fix build flag for smaller binaries

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ apt-get install -y curl default-jdk python wget git build-essential cmake screen
 git clone https://github.com/juj/emsdk.git
 cd emsdk
 ./emsdk update-tags
-./emsdk install --disable-assertions --enable-wasm sdk-incoming-64bit -j20
+./emsdk install --build=Release --enable-wasm sdk-incoming-64bit -j20
 ./emsdk activate sdk-incoming-64bit
